2301bd5438e3ea914a538cb755526a3ecb082ccf
jsartisan <pawan.stardust@gmail.com>
2019-12-18 14:38:24 +0530

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: "react-native",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testPathIgnorePatterns: ["/node_modules/", "lib"]
+  testPathIgnorePatterns: ["/node_modules/", "/lib/"]
 };
