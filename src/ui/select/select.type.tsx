f3eeea70799ec50f9d53e58826cabf0bb674269f
jsartisan <pawan.stardust@gmail.com>
2020-10-14 21:47:07 +0530

    remove children in select component

--- src/ui/select/select.type.tsx
+++ src/ui/select/select.type.tsx
@@ -44,7 +44,6 @@ export interface SelectProps
   left?: number;
   multiple?: boolean;
   value: any;
-  children?: React.ReactElement[] | React.ReactElement;
   footer?: React.ReactElement[] | React.ReactElement;
   onSelect: (value: any) => void;
   data: any[];
