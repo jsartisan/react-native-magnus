4ab87600463f5c372def358d50ddad740c599d61
Pawan Kumar <pawan.stardust@gmail.com>
2020-11-05 22:46:45 +0530

    change order of meta tags and style

--- docs/gatsby-ssr.js
+++ docs/gatsby-ssr.js
@@ -9,3 +9,25 @@ exports.onRenderBody = ({ pathname, setHeadComponents }) => {
     />,
   ]);
 };
+
+exports.onPreRenderHTML = function onPreRenderHTML({
+  getHeadComponents,
+  replaceHeadComponents,
+}) {
+  const headComponents = getHeadComponents();
+  headComponents.sort((a, b) => {
+    if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
+      return 0;
+    }
+
+    if (a.type === 'style') {
+      return 1;
+    } else if (b.type === 'style') {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  replaceHeadComponents(headComponents);
+};
