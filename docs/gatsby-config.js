1144eb4c3a7d7560ee12a564b697eb1c2ac0372f
Pawan Kumar <pawankumar@Pawans-MacBook-Pro.local>
2020-11-06 11:47:01 +0530

    update theme color

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -68,8 +68,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
         icon: `static/images/favicon/favicon-32x32.png`, // This path is relative to the root of the site.
       },
