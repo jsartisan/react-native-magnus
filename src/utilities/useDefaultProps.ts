2a8c135870a84fc59c1db42abfa946f19cf15485
Erick Estevão Riva Pramio <erickriva@hotmail.com.br>
2021-01-05 02:45:35 -0300

    Update useDefaultProps.ts
    Just cleaning code

--- src/utilities/useDefaultProps.ts
+++ src/utilities/useDefaultProps.ts
@@ -7,18 +7,6 @@ export const useDefaultProps = <T>(
 ): T => {
   const theme = useTheme();
 
-  // const latestProps = useRef<T>({ ...defaultProps, ...props });
-  // useEffect(() => {
-  //   const appliedStyles = mergeRecursively(
-  //     theme.components[componentName],
-  //     defaultProps
-  //   );
-
-  //   latestProps.current = mergeRecursively(appliedStyles, props) as T;
-  // });
-
-  // return latestProps.current;
-
   return {
     ...defaultProps,
     ...((theme.components && theme.components[componentName]) ?? {}),
