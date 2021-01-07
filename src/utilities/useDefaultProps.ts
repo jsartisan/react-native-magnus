41c5f126cae73d975309d9aa8b9382316f319923
Erick Estevão Riva Pramio <erickriva@hotmail.com.br>
2021-01-07 16:38:14 -0300

    Update useDefaultProps.ts
    A better typing for contributors. Now, props set on defaultProps can't be undefined.
    Props coming from useDefaultProps will be different from props received by component.

--- src/utilities/useDefaultProps.ts
+++ src/utilities/useDefaultProps.ts
@@ -1,15 +1,27 @@
+import React from 'react';
+import deepmerge from 'deepmerge';
 import { ThemeType, useTheme } from '../theme';
 
-export const useDefaultProps = <T>(
+export const useDefaultProps = <T, DT = Partial<T>>(
   componentName: keyof NonNullable<ThemeType['components']>,
   props: T,
-  defaultProps: Partial<T> = {}
-): T => {
+  defaultProps: DT
+) => {
   const theme = useTheme();
 
-  return {
-    ...defaultProps,
-    ...((theme.components && theme.components[componentName]) ?? {}),
-    ...props,
-  };
+  const finalProps = React.useMemo(() => {
+    // @ts-ignore
+    const propsFromTheme: Partial<T> =
+      (theme.components && theme.components[componentName]) ?? {};
+
+    // @ts-ignore
+    const mergedProps: T & Required<typeof defaultProps> = deepmerge(
+      deepmerge(defaultProps, propsFromTheme),
+      props
+    );
+
+    return mergedProps;
+  }, [componentName, defaultProps, props, theme.components]);
+
+  return finalProps;
 };
