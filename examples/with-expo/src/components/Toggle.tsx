d5a14934f5e1fea8b00c64c5de11164924b26714
jsartisan <pawan.stardust@gmail.com>
2021-02-23 22:09:45 +0530

    lerna setup

--- examples/with-expo/src/components/Toggle.tsx
+++ examples/with-expo/src/components/Toggle.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { ScrollView } from "react-native";
-import { Toggle } from "react-native-magnus";
+import React from 'react';
+import { ScrollView } from 'react-native';
+import { Toggle } from 'react-native-magnus';
 
-import ExamplePage from "../utils/ExamplePage";
-import ExampleHeader from "../utils/ExampleHeader";
-import ExampleSection from "../utils/ExampleSection";
+import ExamplePage from '../utils/ExamplePage';
+import ExampleHeader from '../utils/ExampleHeader';
+import ExampleSection from '../utils/ExampleSection';
 
 const ToggleComponent: React.FC = () => {
   const [on, toggle] = React.useState(false);
---
+++ examples/with-expo/webpack.config.js
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const fs = require('fs');
+const path = require('path');
+
+const node_modules = path.resolve(__dirname, '../..', 'node_modules');
+const packages = path.resolve(__dirname, '../..', 'packages');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  config.context = path.resolve(__dirname, '../..');
+
+  config.module.rules.push({
+    test: /\.(js|ts|tsx)$/,
+    include: /(packages|example)\/.+/,
+    exclude: /node_modules/,
+    use: 'babel-loader',
+  });
+
+  Object.assign(config.resolve.alias, {
+    react: path.resolve(node_modules, 'react'),
+    'react-native': path.resolve(node_modules, 'react-native-web'),
+    'react-native-web': path.resolve(node_modules, 'react-native-web'),
+  });
+
+  fs.readdirSync(packages)
+    .filter((name) => !name.startsWith('.'))
+    .forEach((name) => {
+      console.log(name, packages);
+      config.resolve.alias['react-native-magnus'] = path.resolve(
+        packages,
+        name,
+        require('../../packages/core/package.json').source
+      );
+    });
+
+  return config;
+};
