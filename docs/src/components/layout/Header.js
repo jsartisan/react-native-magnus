ba558b48d8ea17258886ffb98471d07c8762741c
Pawan Kumar <pawan.stardust@gmail.com>
2022-09-07 20:21:39 +0530

    upadte header

--- docs/src/components/layout/Header.js
+++ docs/src/components/layout/Header.js
@@ -3,11 +3,7 @@ import { Link } from 'gatsby';
 
 import Logo from '../common/Logo';
 import Search from '../common/Search';
-import LoginModal from '../common/LoginModal';
-import SubmitModal from '../common/SubmitModal';
-import UserDropdown from '../common/UserDropdown';
 import packageJSON from '../../../../package.json';
-import { isLoggedIn, getUser } from '../../utilities/auth';
 
 export default function Header({
   location,
@@ -16,12 +12,9 @@ export default function Header({
   showSidebarMenu,
 }) {
   const user = getUser();
-  const [loginModalVisible, setLoginModalVisible] = useState(false);
-  const [submitModalVisible, setSubmitModalVisible] = useState(false);
-
   return (
     <div
-      className="px-5 flex fixed top-0 left-0 w-full border-b z-20 w-full"
+      className="fixed top-0 left-0 z-20 flex w-full px-5 border-b"
       style={{
         height: '70px',
         backdropFilter: 'blur(32px)',
@@ -29,81 +22,71 @@ export default function Header({
         borderColor: '#dcdcdc91',
       }}
     >
-      <div className="flex items-center max-w-screen-xl w-full mx-auto">
-        <div className="flex items-center relative">
+      <div className="flex items-center w-full max-w-screen-xl mx-auto">
+        <div className="relative flex items-center">
           <Logo hasText />
-          <div className="text-gray-700 text-xs bg-gray-200 px-1 ml-3 md:ml-3 rounded-sm">
+          <div className="px-1 ml-3 text-xs text-gray-700 bg-gray-200 rounded-sm md:ml-3">
             v{packageJSON.version}
           </div>
         </div>
         <Search />
         <div className="">
-          <ul className="flex items-center ml-3 d-none d-sm-none d-md-flex list-none">
-            <li className="ml-3 hidden md:block">
-              <Link
-                to="/snippets"
-                className="rounded block px-3 py-2 text-gray-900 hover:bg-gray-200"
-              >
-                Snippets
-              </Link>
-            </li>
+          <ul className="flex items-center ml-3 list-none d-none d-sm-none d-md-flex">
             <li className="hidden md:block">
               <Link
                 to="/blog"
-                className="btn-light rounded block px-3 py-2 text-gray-900 hover:bg-gray-200"
+                className="block px-3 py-2 text-gray-900 rounded btn-light hover:bg-gray-200"
               >
                 Blog
               </Link>
             </li>
-            <li className="ml-3 hidden md:block">
+            <li className="hidden ml-3 md:block">
               <Link
                 to="/docs"
-                className="rounded block px-3 py-2 text-gray-900 hover:bg-gray-200"
+                className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-200"
               >
                 Documentation
               </Link>
             </li>
-            {!isLoggedIn() && (
-              <li className="ml-3 hidden md:block">
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setLoginModalVisible(true);
-                  }}
-                  className="rounded block px-3 py-2 text-gray-900 hover:bg-gray-200"
-                >
-                  Login
-                </a>
-                <LoginModal
-                  open={loginModalVisible}
-                  toggle={setLoginModalVisible}
-                />
-              </li>
-            )}
-            {isLoggedIn() && (
-              <>
-                <div className="mt-2 sm:mt-0 flex flex-col sm:flex-row items-center sm:border-l sm:border-gray-300">
-                  <button
-                    title="Submit component"
-                    className="sm:ml-4 py-2 px-5 md:px-6 rounded-lg bg-primary-500 text-white font-bold hidden md:block"
-                    onClick={() => setSubmitModalVisible(true)}
-                  >
-                    Submit your own
-                  </button>
-                </div>
-
-                <UserDropdown user={user} />
-                <SubmitModal
-                  open={submitModalVisible}
-                  toggle={setSubmitModalVisible}
-                />
-              </>
-            )}
+            <li className="block ml-2 md:ml-3">
+              <a
+                href="https://discord.gg/SYnXGEy"
+                target="_blank"
+                className="flex block p-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-600 "
+              >
+                <i className="text-xl icon-discord " />
+              </a>
+            </li>
+            <li className="block ml-1 md:ml-3">
+              <a
+                href="https://github.com/jsartisan/react-native-magnus"
+                target="_blank"
+                className="flex block p-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-600 "
+              >
+                <i className="text-xl icon-github " />
+              </a>
+            </li>
+            <li className="block ml-1 md:ml-3">
+              <a
+                href="https://twitter.com/magnusui"
+                target="_blank"
+                className="flex block p-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-600"
+              >
+                <i className="text-xl icon-twitter" />
+              </a>
+            </li>
+            {/* <li className="hidden ml-3 md:block">
+              <a
+                href="https://truesight.one"
+                className="block px-4 py-2 text-gray-900 bg-white border rounded hover:bg-gray-200"
+              >
+                Hire Us
+              </a>
+            </li> */}
             {showSidebarMenu && (
               <li>
                 <button
-                  className="block md:hidden ml-3 flex"
+                  className="flex block ml-3 md:hidden"
                   onClick={() => setSidebarOpen(!sidebarOpen)}
                 >
                   <i
